80e489b0d28e821ee2d2dee4786aa801d42d277f
Kateryna <katepysova97@gmail.com>
2023-05-11 21:23:29 +0200

    Added small fixes

--- src/js/form-validation.js
+++ src/js/form-validation.js
@@ -3,7 +3,6 @@ const textareas = document.querySelectorAll("textarea");
 textareas.forEach((textarea) => {
   textarea.addEventListener("touchstart", (event) => {
     event.stopPropagation();
-    event.preventDefault();
   });
 });
 
